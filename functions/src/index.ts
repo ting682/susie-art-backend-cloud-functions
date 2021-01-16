d4c1498c3182223f0ce6fc4855d3f076a00810c8
ting682 <tchung682@gmail.com>
2021-01-16 10:25:15 -0500

    working on cart

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -33,11 +33,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(__dirname));
 
-const exampleVar = process.env.SANDBOX_ID
-app.get("/", function (req :any, res :any) {
-    // res.sendFile(__dirname + '/index.html');
-    res.render("home", {exampleVar}) 
-   })
+// const exampleVar = process.env.SANDBOX_ID
+// app.get("/", function (req :any, res :any) {
+//     // res.sendFile(__dirname + '/index.html');
+//     res.render("home", {exampleVar}) 
+//    })
 
 // Initialized the Square api client:
 //   Set sandbox environment for testing purpose
@@ -47,7 +47,7 @@ const client = new Client({
   accessToken: functions.config().square.accesstoken,
 });
 
-app.post('/', async (req: any, res: any) => {
+app.post('/payments', async (req: any, res: any) => {
   const requestParams = req.body;
 
   
@@ -58,7 +58,7 @@ app.post('/', async (req: any, res: any) => {
     sourceId: requestParams.nonce,
     amountMoney: {
       amount: requestParams.amount, // $1.00 charge
-      currency: 'USD'
+      currency: 'USD',
     },
 
     locationId: requestParams.location_id,
@@ -69,7 +69,7 @@ app.post('/', async (req: any, res: any) => {
     const response = await paymentsApi.createPayment(requestBody);
     res.status(200).json({
       'title': 'Payment Successful',
-      'result': response.result
+      'result': response.result,
     });
   } catch(error) {
     let errorResult = null;
@@ -80,10 +80,96 @@ app.post('/', async (req: any, res: any) => {
     }
     res.status(500).json({
       'title': 'Payment Failure',
-      'result': errorResult
+      'result': errorResult,
     });
   }
 });
 
 
 exports.payments = functions.https.onRequest(app)
+
+// import { v4 as uuidv4 } from 'uuid'
+const jwt = require('jsonwebtoken')
+
+const cart = express();
+cart.use(cors({origin: true, preflightContinue: true}))
+
+const cookieParser = require('cookie-parser');
+cart.use(cookieParser());
+
+cart.set('view engine', 'ejs'); 
+
+cart.use(bodyParser.json());
+cart.use(bodyParser.urlencoded({ extended: false }));
+
+// cart.use(function(req :any, res :any, next :any) {
+//   req.setHeader("Access-Control-Allow-Origin", "http://localhost:3002");
+//   req.setHeader('Access-Control-Allow-Credentials', "true")
+//   req.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   req.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   next();
+// });
+
+cart.post('/carts', cors({origin: true}), (req: any, res: any) => {
+
+  const requestParams = req.body;
+
+  const cartsRef = admin.database().ref('carts/' + requestParams.uid)
+
+  cartsRef.set({
+    uid: requestParams.uid,
+    lineItems: requestParams.lineItems,
+  }).then(resp => {
+    
+    const jwtToken = jwt.sign({uid: requestParams.uid,  lineItems: requestParams.lineItems}, functions.config().jwt.secret)
+
+    res.status(200).cookie('authcookie', jwtToken , { maxAge:900000, httpOnly:true }).send({jwtToken})
+    
+  }).catch(err => {
+    res.json({error: err})
+    res.status(500).send()
+  })
+
+  
+
+  // let userRef = admin.database().ref('users/' + requestParams.uid)
+
+  // userRef.update({
+  //   uid: requestParams.uid
+
+  // })
+  
+})
+
+
+cart.get('/carts', async (req: any, res: any) => {
+
+  res.render('index.html')
+
+  // const authCookie = req.cookies.authcookie
+
+  // jwt.verify(authCookie, functions.config().jwt.secret, (err :any, data :any) => {
+  //   if(err){
+  //     res.sendStatus(403)
+  //   } 
+  //   else if(data.uid){
+  //     req.uid = data.uid
+
+  //     const cartsRef = admin.database().ref('carts/' + data.uid)
+
+
+  //     cartsRef.once('value').then(snap => {
+  //       res.send(JSON.stringify({lineItems: snap.val().lineItems}))
+  //     }).catch(errorData => {
+  //       res.json({error: errorData})
+  //     })
+      
+  //  }
+  // })
+
+
+})
+
+// const corsHandler = cors({origin: "http://localhost:3002", credentials: true})
+
+exports.carts = functions.https.onRequest(cart)
