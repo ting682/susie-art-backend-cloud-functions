de53ecd2353afcabe1e6ddf980a387e63fa5e482
ting682 <tchung682@gmail.com>
2021-02-02 10:10:50 -0500

    added order locale date

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -339,24 +339,16 @@ cart.patch('/', async (req :any, res :any) => {
 
 })
 
-// const corsHandler = cors({origin: "http://localhost:3002", credentials: true})
+
 
 exports.carts = functions.https.onRequest(cart)
 
 
 // const nodemailer = require('nodemailer')
 
-// const { google } = require('googleapis')
 
 // const aws = require('aws-sdk')
 
-// const gmailEmail = functions.config().gmail.email;
-
-// const gmailPassword = functions.config().gmail.password;
-
-// const oAuth2Client = new google.auth.OAuth2(functions.config().google.clientid, functions.config().google.clientsecret, functions.config().google.redirecturi)
-
-// oAuth2Client.setCredentials({refresh_token: functions.config().google.refreshtoken})
 
 // aws.config.update({
 //   accessKeyId: functions.config().aws.accesskeyid,
@@ -366,7 +358,8 @@ exports.carts = functions.https.onRequest(cart)
 
 const mailgun = require("mailgun-js");
 
-const DOMAIN = "sandbox33c8b0872ffa4b329818517167470c60.mailgun.org";
+// const DOMAIN = "sandbox33c8b0872ffa4b329818517167470c60.mailgun.org";
+const DOMAIN = "mail.susiewang.art"
 
 exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}').onCreate(async (snapshot, context) => {
 
@@ -464,27 +457,7 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
   
   </head>
   <body itemscope itemtype="http://schema.org/EmailMessage" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6">
-  <table width="100%" border="0" cellspacing="0" cellpadding="0" class="em_full_wrap" align="center"  bgcolor="#efefef">
-      <tr>
-        <td align="center" valign="top"><table align="center" width="650" border="0" cellspacing="0" cellpadding="0" class="em_main_table" style="width:650px; table-layout:fixed;">
-            <tr>
-              <td align="center" valign="top" style="padding:0 25px;" class="em_aside10"><table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-                <tr>
-                  <td height="26" style="height:26px;" class="em_h20">&nbsp;</td>
-                </tr>
-                <tr>
-                  <td align="center" valign="top"><a href="#" target="_blank" style="text-decoration:none;"><img src="/assets/pilot/images/templates/header_logo.png" width="208" height="41" alt="meowgun" border="0" style="display:block; font-family:Arial, sans-serif; font-size:18px; line-height:25px; text-align:center; color:#1d4685; font-weight:bold; max-width:208px;" class="em_w150" /></a></td>
-                </tr>
-                <tr>
-                  <td height="28" style="height:28px;" class="em_h20">&nbsp;</td>
-                </tr>
-              </table>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-  </table>
+  
   <table width="100%" border="0" cellspacing="0" cellpadding="0" class="em_full_wrap" align="center" bgcolor="#efefef">
       <tr>
         <td align="center" valign="top" class="em_aside5"><table align="center" width="650" border="0" cellspacing="0" cellpadding="0" class="em_main_table" style="width:650px; table-layout:fixed;">
@@ -500,20 +473,20 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
                   <td height="22" style="height:22px;" class="em_h20">&nbsp;</td>
                 </tr>
                 <tr>
-                  <td class="em_blue em_font_22" align="center" valign="top" style="font-family: Arial, sans-serif; font-size: 26px; line-height: 29px; color:#264780; font-weight:bold;">Your Cat Thanks You</td>
+                  <td class="em_blue em_font_22" align="center" valign="top" style="font-family: Arial, sans-serif; font-size: 26px; line-height: 29px; color:#264780; font-weight:bold;">Susie Wang Art Thanks You</td>
                 </tr>
                 <tr>
                   <td height="15" style="height:15px; font-size:0px; line-height:0px;">&nbsp;</td>
                 </tr>
                 <tr>
-                  <td class="em_grey" align="center" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 22px; color:#434343;">Fluffy, thanks so much for choosing Meowgun for your litter box needs.<br class="em_hide" />
-  We’ve received your order, and the herding of cats has&nbsp;begun.</td>
+                  <td class="em_grey" align="center" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 22px; color:#434343;">${order.billing.firstName}, thanks so much for choosing Susie Wang Art Studio.<br class="em_hide" />
+  We’ve received your order, and will begin processing and notify you of your status within 48 hours.&nbsp;</td>
                 </tr>
                 <tr>
                   <td height="15" style="height:15px; font-size:1px; line-height:1px;">&nbsp;</td>
                 </tr>
                 <tr>
-                  <td class="em_grey" align="center" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 22px; color:#434343;"><strong>Order #:</strong> <span style="color:#da885b; text-decoration:underline;">${context.params.orderId}</span> <span class="em_hide2">&nbsp;|&nbsp;</span><span class="em_mob_block"></span> <strong>Order Date:</strong> October 29, 2019</td>
+                  <td class="em_grey" align="center" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 22px; color:#434343;"><strong>Order #:</strong> <span style="color:#da885b; text-decoration:underline;">${context.params.orderId}</span> <span class="em_hide2">&nbsp;|&nbsp;</span><span class="em_mob_block"></span> <strong>Order Date:</strong> ${order.orderLocaleDate}</td>
                 </tr>
                 <tr>
                   <td height="20" style="height:20px; font-size:1px; line-height:1px;">&nbsp;</td>
@@ -607,7 +580,7 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
                           <td>&nbsp;</td>
                           <td class="em_grey" width="100" align="right" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 20px; color:#434343; width:100px; font-weight:bold;">Total</td>
                           <td width="20" style="width:20px; font-size:0px; line-height:0px;"></td>
-                          <td width="100" class="em_grey" align="right" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 20px; color:#434343; width:100px; font-weight:bold;">$${order.totalPrice/100}.${order.totalPrice % 100}</td>
+                          <td width="100" class="em_grey" align="right" valign="top" style="font-family: Arial, sans-serif; font-size: 16px; line-height: 20px; color:#434343; width:100px; font-weight:bold;">$${(order.totalPrice / 100).toFixed(2)}</td>
                         </tr>
                       </table>
                   </td>
@@ -633,11 +606,12 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
                 <tr>
                   <td align="center" valign="top"><table border="0" cellspacing="0" cellpadding="0" align="center">
                       <tr>
-                        <td width="30" style="width:30px;" align="center" valign="middle"><a href="#" target="_blank" style="text-decoration:none;"><img src="/assets/pilot/images/templates/fb.png" width="30" height="30" alt="Fb" border="0" style="display:block; font-family:Arial, sans-serif; font-size:18px; line-height:25px; text-align:center; color:#000000; font-weight:bold; max-width:30px;" /></a></td>
+                        <td width="30" style="width:30px;" align="center" valign="middle"><a href="https://www.facebook.com/Susie.Ketty.Riley" target="_blank" style="text-decoration:none;"><img src="https://firebasestorage.googleapis.com/v0/b/susie-wang-art.appspot.com/o/icons%2Ffacebook_icon.png?alt=media&token=deea5478-a4b5-4a07-9bf7-d53d63737556" width="30" height="30" alt="Fb" border="0" style="display:block; font-family:Arial, sans-serif; font-size:18px; line-height:25px; text-align:center; color:#000000; font-weight:bold; max-width:30px;" /></a></td>
                         <td width="12" style="width:12px;">&nbsp;</td>
-                        <td width="30" style="width:30px;" align="center" valign="middle"><a href="#" target="_blank" style="text-decoration:none;"><img src="/assets/pilot/images/templates/tw.png" width="30" height="30" alt="Tw" border="0" style="display:block; font-family:Arial, sans-serif; font-size:14px; line-height:25px; text-align:center; color:#000000; font-weight:bold; max-width:30px;" /></a></td>
+                        <td width="30" style="width:30px;" align="center" valign="middle"><a href="https://twitter.com/SusieWangCFA" target="_blank" style="text-decoration:none;"><img src="https://firebasestorage.googleapis.com/v0/b/susie-wang-art.appspot.com/o/icons%2Ftwitter_icon.png?alt=media&token=3ca35199-0cfa-4bf4-9d56-ac9c0a8b2169" width="30" height="30" alt="Insta" border="0" style="display:block; font-family:Arial, sans-serif; font-size:14px; line-height:25px; text-align:center; color:#000000; font-weight:bold; max-width:30px;" /></a></td>
                         <td width="12" style="width:12px;">&nbsp;</td>
-                        <td width="30" style="width:30px;" align="center" valign="middle"><a href="#" target="_blank" style="text-decoration:none;"><img src="/assets/pilot/images/templates/insta.png" width="30" height="30" alt="Insta" border="0" style="display:block; font-family:Arial, sans-serif; font-size:14px; line-height:25px; text-align:center; color:#000000; font-weight:bold; max-width:30px;" /></a></td>
+                        <td width="30" style="width:30px;" align="center" valign="middle"><a href="https://www.instagram.com/susie_wang26/" target="_blank" style="text-decoration:none;"><img src="https://firebasestorage.googleapis.com/v0/b/susie-wang-art.appspot.com/o/icons%2Finstagram_icon.png?alt=media&token=99516536-8eca-4e9b-830f-4c32e370a8e1" width="30" height="30" alt="Tw" border="0" style="display:block; font-family:Arial, sans-serif; font-size:14px; line-height:25px; text-align:center; color:#000000; font-weight:bold; max-width:30px;" /></a></td>
+      
                       </tr>
                     </table>
                    </td>
@@ -654,7 +628,7 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
                 <tr>
                   <td align="center" valign="top" style="font-size:0px; line-height:0px;"><table border="0" cellspacing="0" cellpadding="0" align="center">
                     <tr>
-                      <td width="15" align="left" valign="middle" style="font-size:0px; line-height:0px; width:15px;"><a href="mailto:meow@meowgun.com" style="text-decoration:none;"><img src="/assets/pilot/images/templates/email_img.png" width="15" height="12" alt="" border="0" style="display:block; max-width:15px;" /></a></td>
+                      <td width="15" align="left" valign="middle" style="font-size:0px; line-height:0px; width:15px;"><a href="mailto:susiewangart@gmail.com" style="text-decoration:none;"><img src="https://firebasestorage.googleapis.com/v0/b/susie-wang-art.appspot.com/o/icons%2Femail_icon.png?alt=media&token=54c910a3-67cd-42b3-91ec-53ad67e85a5f" width="15" height="12" alt="" border="0" style="display:block; max-width:15px;" /></a></td>
                       <td width="9" style="width:9px; font-size:0px; line-height:0px;" class="em_w5"><img src="/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block;" /></td>
                       <td class="em_grey em_font_11" align="left" valign="middle" style="font-family: Arial, sans-serif; font-size: 13px; line-height: 15px; color:#434343;"><a href="mailto:meow@meowgun.com" style="text-decoration:none; color:#434343;">meow@meowgun.com</a> <a href="mailto:marketing@mailgun.com" style="text-decoration:none; color:#434343;">[mailto:marketing@mailgun.com]</a></td>
                     </tr>
@@ -691,7 +665,7 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
                 <tr>
                   <td align="center" valign="top"><table border="0" cellspacing="0" cellpadding="0" align="left" class="em_wrapper">
                     <tr>
-                      <td class="em_grey" align="center" valign="middle" style="font-family: Arial, sans-serif; font-size: 11px; line-height: 16px; color:#434343;">&copy; Meowgun 2019  &nbsp;|&nbsp;  <a href="#" target="_blank" style="text-decoration:underline; color:#434343;">Unsubscribe</a></td>
+                      <td class="em_grey" align="center" valign="middle" style="font-family: Arial, sans-serif; font-size: 11px; line-height: 16px; color:#434343;">&copy; Susie Wang Art Studios 2021  &nbsp;|&nbsp;  <a href="%mailing_list_unsubscribe_url%" target="_blank" style="text-decoration:underline; color:#434343;"></a>If you wish to unsubscribe, please click the unsubscribe link below.</td>
                     </tr>
                   </table>
                   </td>
@@ -713,9 +687,10 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
   </html>`
 
   const data = {
-    from: "Susie Wang art  <postmaster@sandbox33c8b0872ffa4b329818517167470c60.mailgun.org>",
+    // from: "Susie Wang art  <postmaster@sandbox33c8b0872ffa4b329818517167470c60.mailgun.org>",
+    from: "Susie Wang Art <noreply@mail.susiewang.art>",
     to: "tchung682@gmail.com",
-    subject: 'Order for Susie Wang Art confirmation email',
+    subject: 'Susie Wang Art order confirmation ' + context.params.orderId,
     text: 'Thank you for your order.' + context.params.orderId,
     html: '<h1>Susie Wang Art</h1>' + orderhtml,
   };
@@ -727,23 +702,7 @@ exports.sendOrderEmailConfirmation = functions.database.ref('/orders/{orderId}')
     console.log(body);
   });
   
-  // const accessToken = await oAuth2Client.getAccessToken()
 
-  // const mailTransport = nodemailer.createTransport({
-  // service: 'gmail',
-  // auth: {
-  //   // type: 'OAuth2',
-  //   // clientId: functions.config().google.clientid,
-  //   // clientSecret: functions.config().google.clientsecret,
-  //   // refreshToken: functions.config().google.refreshtoken,
-  //   // accessToken: accessToken,
-  //   user: gmailEmail,
-  //   pass: gmailPassword,
-  // },
-  // tls: {
-  //   rejectUnauthorized: false
-  // }
-  // })
 
   // let ses = new aws.SES({
   //   apiVersion: '2010-12-01'
